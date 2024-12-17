80e12bcaaa1c715b310f49a48c9a4f20280f687d
EdisonZheng2323 <edisonzheng3737@gmail.com>
2024-12-17 17:46:40 -0500

    Make the square random color and fix the grid size

--- javascript.js
+++ javascript.js
@@ -1,29 +1,34 @@
-let gridSize = 16;
+let initialGridSize = 16;
 let container = document.querySelector(".container");
 function createGrid(gridSize){
   for(let i = 0; i < gridSize * gridSize; i++){
     let square = document.createElement("div");
-    let width = window.innerWidth;
-    let height = window.innerHeight;
-    let squareWidth = width/gridSize + "px";
-    let squareHeight = height/gridSize + "px";
+    let squareWidth = 800/gridSize + "px";
+    let squareHeight = 800/gridSize + "px";
     square.style.width = squareWidth;
     square.style.height = squareHeight;
+    square.style.borderStyle = 'solid';
+    square.style.borderColor = 'gray';
+
     square.addEventListener('mouseover', function() {
-      square.classList.add('hovered');
+      square.style.backgroundColor = getRandomColor();
     })
     container.appendChild(square);
   }
 }
+function getRandomColor(){
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+}
 let reset = document.querySelector("#reset");
 reset.addEventListener('click', function() {
-  let resize = prompt("How many number of square per side of the grid do you want? (Put a number between 1 and 100)");
-  while(+resize > 100 || +resize < 1){
-    resize = prompt("How many number of square per side of the grid do you want? (Put a number between 1 and 100)");
+  let resize = Number(prompt("How many number of square per side of the grid do you want? (Put a number between 1 and 100)"));
+  while(resize > 100 || resize < 1 || isNaN(resize) || !Number.isInteger(resize)){
+    resize = Number(prompt("How many number of square per side of the grid do you want? (Put a number between 1 and 100)"));
   }
-  resize = +resize;
   container.innerHTML = '';
   createGrid(resize);
-
 })
-createGrid(gridSize);
+createGrid(initialGridSize);
